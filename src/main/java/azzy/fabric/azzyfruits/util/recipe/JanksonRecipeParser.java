f26920d41bcd89c73de82d90ef68daca3e43588e
JV <git@jvsrivas.com>
2020-07-03 15:57:02 -0400

    Modified parser to change the way config files are saved from URL.  THIS ISN'T COMPLETE YET.

--- src/main/java/azzy/fabric/azzyfruits/util/recipe/JanksonRecipeParser.java
+++ src/main/java/azzy/fabric/azzyfruits/util/recipe/JanksonRecipeParser.java
@@ -1,13 +1,12 @@
 package azzy.fabric.azzyfruits.util.recipe;
 
-import azzy.fabric.azzyfruits.util.recipe.base.RecipeIntermediary;
 import blue.endless.jankson.*;
 import blue.endless.jankson.impl.Marshaller;
 import blue.endless.jankson.impl.SyntaxError;
+import net.fabricmc.loader.api.FabricLoader;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 import java.util.stream.Stream;
@@ -19,22 +18,31 @@ public class JanksonRecipeParser {
 
     private static Jankson recipeLoader;
     private static volatile File recipes;
+    private static String baseURL = "";
 
     public static void init(){
         recipeLoader = Jankson.builder().build();
 
         // Get the config folder path.
-        recipes = new File("config/azzyfruits");
+        File configDirectory = FabricLoader.getInstance().getConfigDirectory();
+        if(!configDirectory.exists()) {
+            // Severe error.  Minecraft config folder doesn't exist.  Report error to Fabric!!!
+            FFLog.fatal("Minecraft config folder: '" + configDirectory.getAbsolutePath() + "' doesn't exist!  Please report error to Fabric!");
+
+            // Not sure if I should kill the entire instance.  But let's give this a try.  I can't imagine minecraft running without any config.
+            System.exit(-1);
+        }
+        recipes = new File(configDirectory, "forgottenfruits");
 
         if((config.isRegenOn()) && recipes.exists()) {
             // Folder exists and needs to be regenerated.  Do a recursive drop of the folder.
             try {
                 FileUtils.deleteDirectory(recipes);
                 if(!recipes.mkdir()) {
-                    FFLog.error("Unable to create azzyfruits config folder.");
+                    FFLog.error("Unable to create 'forgottenfruits' config folder.");
                 }
             } catch (IOException e) {
-                FFLog.error("Unable to delete azzyfruits config folder to regenerate", e);
+                FFLog.error("Unable to delete 'forgottenfruits' config folder to regenerate", e);
             }
         }
 
@@ -42,69 +50,12 @@ public class JanksonRecipeParser {
     }
 
     private static void validateRecipeCategories(){
-        JsonObject recipeJson;
-        Class recipeReflection;
-        Object recipeInstance;
-        File recipeData;
-        FileWriter writer;
+        // Damn, this is a hard way to get the version number.
+        net.fabricmc.loader.api.metadata.ModMetadata modMetaData = FabricLoader.getInstance().getModContainer("azzyfruits").get().getMetadata();
+        net.fabricmc.loader.api.Version version = modMetaData.getVersion();
+        String versionStr = version.getFriendlyString();
 
-        for (int i = 0; i < RecipeRegistryKey.values().length; i++){
-            String recipeType = RecipeRegistryKey.values()[i].name();
-            recipeJson = null;
-            recipeReflection = null;
-            recipeInstance = null;
-
-
-            if(new File(recipes.getPath()+"\\"+recipeType+".json5").exists()){
-                try {
-                    recipeJson = recipeLoader.load(new File(recipes.getPath()+"\\"+recipeType+".json5"));
-                } catch (IOException | SyntaxError e) {
-                    e.printStackTrace();
-                }
-            }
-            try {
-                recipeReflection = Class.forName("azzy.fabric.azzyfruits.util.recipe.base."+recipeType);
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-            if(recipeReflection != null){
-                try {
-                    recipeInstance = recipeReflection.newInstance();
-                } catch (InstantiationException | IllegalAccessException e) {
-                    e.printStackTrace();
-                }
-
-                if(!(recipeJson != null && dataCheck(recipeJson))){
-                    if(recipeJson != null){
-                        new File(recipes.getPath()+"\\"+recipeType+".json5").delete();
-                    }
-
-                    recipeData = new File(recipes.getPath()+"\\"+recipeType+".json5");
-
-                    recipeJson = new JsonObject();
-                    JsonArray recipeBuffer = new JsonArray();
-
-                    String[] buffered = ((RecipeIntermediary) recipeInstance).getRecipes();
-
-                    for (String element : buffered) {
-                        recipeBuffer.add(new JsonPrimitive(element));
-                    }
-                    Optional<String> notes = Optional.ofNullable(((RecipeIntermediary) recipeInstance).getInfo());
-                    recipeJson.put("Notes", new JsonPrimitive(notes.orElse("")));
-                    recipeJson.put("enabled", new JsonPrimitive(true));
-                    recipeJson.put("type", new JsonPrimitive(recipeType));
-                    recipeJson.put("recipes", recipeBuffer);
-
-                    try {
-                        writer = new FileWriter(recipeData);
-                        writer.write(recipeJson.toJson(true, true));
-                        writer.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
+        FileUtils.copyURLToFile();
     }
 
     public static Queue<Iterator<String>> getRecipeQueue(RecipeRegistryKey key){
